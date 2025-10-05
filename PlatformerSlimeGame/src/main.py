9a08c9f6cbc4f34937382e40c7897ffa231b054a
Phạm Duy Khang <phamduykhang197@gmail.com>
2025-10-05 12:58:58 +0700

    Add load list image

--- PlatformerSlimeGame/src/main.py
+++ PlatformerSlimeGame/src/main.py
@@ -1,6 +1,6 @@
 import pygame
 import sys
-from utils import load_image
+from utils import load_image, load_images
 
 
 FPS = 30
@@ -10,7 +10,7 @@ class Game:
         pygame.display.set_caption('Rimuru - Slime')
         
         self.screen = pygame.display.set_mode((640, 480), pygame.RESIZABLE)
-        self.display = pygame.Surface((320, 240))
+        self.display = pygame.Surface((320, 240), pygame.SRCALPHA)
         
         self.clock = pygame.time.Clock()
         
@@ -18,16 +18,25 @@ class Game:
         # Create assest dictionary for image
         self.assets = {
             # Background
-            'background': load_image('ui/backgrounds/background.png')
+            'background': load_image('ui/backgrounds/background.png'),
+            'player/idle': load_images('sprites/player/run', 1),
         }
 
+        self.count = 0
         
 
     def run(self):
-        # blit background 
-        self.display.blit(self.assets['background'], (0, 0))
-        
         while True:
+            self.display.fill((0, 0, 0, 0))
+            
+            # blit background 
+            self.display.blit(self.assets['background'], (0, 0))
+            
+            # blit character
+            self.display.blit(self.assets['player/idle'][self.count], (50, 50))
+            self.count += 1
+            if self.count >= len(self.assets['player/idle']):
+                self.count = 0
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
--- PlatformerSlimeGame/src/utils.py
+++ PlatformerSlimeGame/src/utils.py
@@ -7,5 +7,15 @@ def load_image(path, scale = 1):
     img = pygame.image.load(IMG_PATH + path).convert()
     img = pygame.transform.scale(img, (img.get_width()/scale, img.get_height()/scale))
     
+    # set background (black) -> transparent
+    img.set_colorkey((0, 0, 0))
     return img
 
+
+def load_images(path, scale = 1):
+    images = []
+    listDirImage = sorted(os.listdir(IMG_PATH + path))
+    for imgFileName in listDirImage:
+        images.append(load_image(path+"/"+imgFileName))
+    
+    return images
